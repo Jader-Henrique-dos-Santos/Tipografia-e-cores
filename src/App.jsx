f593d310e63b956af3c5006c61f46c114a62db40
Jader Henrique dos Santos <jh5515785@gmail.com>
2026-01-28 22:59:50 -0300

    adição de arquivos e pastas importantes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function Texto4() {
+  return <p className="texto_4">Gostou? Deixe um comentário</p>;
+}
+
+function App() {
+  return (
+    <>
+      <Texto4 />
+    </>
+  )
+}
+
+export default App
